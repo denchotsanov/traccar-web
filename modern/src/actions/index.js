b83cd485a3ba6571e7998053c9770649ce8393e7
Dencho Tsanov <tsanov.dencho@gmail.com>
2020-02-15 12:06:12 +0200

    update actions

--- modern/src/actions/index.js
+++ modern/src/actions/index.js
@@ -11,4 +11,28 @@ export const updatePositions = positions => ({
 export const updateEvents = events => ({
   type: 'UPDATE_EVENTS',
   events
-})
+});
+
+export const updateServer = server => ({
+  type: 'UPDATE_SERVER',
+  server
+});
+
+export const updateGroups = groups => ({
+  type: 'UPDATE_GROUPS',
+  groups
+});
+
+export const updateSession = session => ({
+  type: 'UPDATE_SESSION',
+  session
+});
+
+export const convertDateTime = function(time,tz) {
+    var date = new Date(time);
+    var timeOptions = {
+        hour12: false,
+        timezone: tz
+        };
+    return date.toLocaleString('bg-BG',timeOptions)
+};
